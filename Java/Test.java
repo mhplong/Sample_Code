c6219426b02f64eae0f58e62ce1cea829d3890bc
mhplong <mhplong@windstream.net>
2015-06-19 20:38:37 -0500

    Update Test.java

--- Java/Test.java
+++ Java/Test.java
@@ -1,5 +1,7 @@
 import System.io.*;
 
-public static void main(String[] args) {
-  System.out.println("Hello World");
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
